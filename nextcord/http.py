da0dabf57f50c89e37f73bf971e8ac30d136f6cb
ooliver1 <oliverwilkes2006@icloud.com>
2022-11-06 21:46:47 +0000

    feat: implement default_thread_slowmode_delay for text and forum

--- nextcord/http.py
+++ nextcord/http.py
@@ -999,6 +999,7 @@ class HTTPClient:
             "default_auto_archive_duration",
             "flags",
             "default_sort_order",
+            "default_thread_rate_limit_per_user",
         )
         payload = {k: v for k, v in options.items() if k in valid_keys}
         return self.request(r, reason=reason, json=payload)
@@ -1039,6 +1040,7 @@ class HTTPClient:
             "video_quality_mode",
             "auto_archive_duration",
             "default_sort_order",
+            "default_thread_rate_limit_per_user",
         )
         payload.update({k: v for k, v in options.items() if k in valid_keys and v is not None})
 
